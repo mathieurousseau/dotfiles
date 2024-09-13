2befb3fcfd5f0c684413c1acad23bfb0848704af
mathieurousseau <mathieu.rousseau@thescore.com>
2024-09-13 01:58:15 -0400

    set oil not lazy

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -214,11 +214,19 @@ return {
   },
   {
     'stevearc/oil.nvim',
+    lazy = false,
     opts = {},
     -- dependencies = { 'nvim-tree/nvim-web-devicons' },
     keys = {
       { '-', '<cmd>Oil<cr>', desc = 'Open parent directory' },
     },
+    config = function()
+      require("oil").setup {
+        view_options = {
+          show_hidden = true,
+        }
+      }
+    end
   },
   {
     'mfussenegger/nvim-lint',
