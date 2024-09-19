c614631d791bd94242b1dd820829652f19bc1bef
mathieurousseau <mathieu.rousseau@thescore.com>
2024-09-19 16:49:05 -0400

    show hidden file in neotree

--- .config/nvim/lua/kickstart/plugins/neo-tree.lua
+++ .config/nvim/lua/kickstart/plugins/neo-tree.lua
@@ -18,19 +18,14 @@ return {
       window = {
         mappings = {
           ['\\'] = 'close_window',
-        },
-        filtered_items = {
-          visible = true,
-          show_hidden_count = true,
-          hide_dotfiles = false,
-          hide_gitignored = true,
-          hide_by_name = {
-            -- '.git',
-            -- '.DS_Store',
-            -- 'thumbs.db',
-          },
-          never_show = {},
-        },
+        }
+      },
+      filtered_items = {
+        visible = true,
+        show_hidden_count = false,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+        never_show = {},
       },
     },
   },
