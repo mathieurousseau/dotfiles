b02d748aa6fa2622a26c77dd5ef54a3b94129f05
mathieurousseau <mathieu.rousseau@thescore.com>
2024-10-03 17:37:25 -0400

    Better folding conf

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -166,6 +166,11 @@ vim.opt.scrolloff = 10
 -- Folding support
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldcolumn = "0"
+vim.opt.foldtext = ""
+vim.opt.foldlevel = 99
+vim.opt.foldlevelstart = 3
+vim.opt.foldnestmax = 4
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
