d5818cd8bae3322775f6628bbf67210b321f3925
mathieurousseau <mathieu.rousseau@thescore.com>
2024-10-03 23:41:00 -0400

    Carbon fox theme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -172,6 +172,9 @@ vim.opt.foldlevel = 99
 vim.opt.foldlevelstart = 3
 vim.opt.foldnestmax = 4
 
+-- Theme
+vim.opt.termguicolors = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 --
@@ -848,13 +851,16 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    -- 'folke/tokyonight.nvim',
+    'EdenEast/nightfox.nvim',
+
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'carbonfox'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
