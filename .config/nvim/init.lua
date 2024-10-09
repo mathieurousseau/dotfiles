e5c5cc8656ab11b4f8b69b43073b6d5de9a9f63c
mathieurousseau <mathieu.rousseau@thescore.com>
2024-10-09 02:21:01 -0400

    fix nvim colorscheme

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -874,7 +874,7 @@ require('lazy').setup({
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.colorscheme 'carbonfox'
+      vim.cmd.colorscheme 'kanagawa-dragon'
       -- vim.cmd.colorscheme 'main'
 
       -- You can configure highlights by doing something like:
