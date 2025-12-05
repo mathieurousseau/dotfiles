6e98d1fe90ba9fd8e1debcf6b7db0042c424478c
Mathieu-Rousseau_pennent <Mathieu.Rousseau@pennentertainment.com>
2025-12-05 15:13:00 -0500

    delete old telescope config

--- .config/t_nvim/lua/custom/plugins/hardtime.lua
+++
@@ -1,9 +0,0 @@
--- Enforce good nvim habits by limiting certain key usage
-return {
-  {
-    "m4xshen/hardtime.nvim",
-    lazy = false,
-    dependencies = { "MunifTanjim/nui.nvim" },
-    opts = {},
-  }
-}
--- .config/t_nvim/lua/custom/plugins/vim-notify.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "rcarriga/nvim-notify",
-  opts = {
-    render = "minimal",
-    top_down = false,
-    max_height = 3
-  },
-}
