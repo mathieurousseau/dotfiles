b47358bf83f05372f5eb5c15ee269a4f5465d3d7
Mathieu-Rousseau_pennent <Mathieu.Rousseau@pennentertainment.com>
2025-12-25 21:51:34 -0500

    nvim improvements

--- .config/nvim/lua/custom/plugins/snacks.lua
+++ .config/nvim/lua/custom/plugins/snacks.lua
@@ -161,9 +161,9 @@ return {
         -- Create some toggle mappings
         Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
         Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-        Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
+        Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>ul")
         Snacks.toggle.diagnostics():map("<leader>ud")
-        Snacks.toggle.line_number():map("<leader>ul")
+        Snacks.toggle.line_number():map("<leader>uL")
         Snacks.toggle.option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 }):map(
           "<leader>uc")
         Snacks.toggle.treesitter():map("<leader>uT")
